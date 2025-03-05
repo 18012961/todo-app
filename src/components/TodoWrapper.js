7ae742e905326aca1875db56099388ae0f7b07e9
18012961 <fortunaterefilwe@gmail.com>
2025-03-05 11:13:01 +0200

    i have installed the database to store my data

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,31 +1,60 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Todo } from "./Todo";
 import { TodoForm } from "./TodoForm";
-import { v4 as uuidv4 } from "uuid";
 import { EditTodoForm } from "./EditTodoForm";
 
-// Default export for TodoWrapper
 const TodoWrapper = () => {
   const [todos, setTodos] = useState([]);
 
+  // Fetch todos from the API on component mount
+  useEffect(() => {
+    fetch("http://localhost:5000/todos")
+      .then((response) => response.json())
+      .then((data) => setTodos(data));
+  }, []);
+
   // Function to add a new Todo
   const addTodo = (todo) => {
-    setTodos([
-      ...todos,
-      { id: uuidv4(), task: todo, completed: false, isEditing: false },
-    ]);
+    const newTodo = { task: todo, completed: false, isEditing: false };
+
+    fetch("http://localhost:5000/todos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newTodo),
+    })
+      .then((response) => response.json())
+      .then((data) => setTodos((prevTodos) => [...prevTodos, data]));
   };
 
   // Function to delete a Todo
-  const deleteTodo = (id) => setTodos(todos.filter((todo) => todo.id !== id));
+  const deleteTodo = (id) => {
+    fetch(`http://localhost:5000/todos/${id}`, {
+      method: "DELETE",
+    }).then(() => {
+      setTodos(todos.filter((todo) => todo.id !== id));
+    });
+  };
 
   // Function to toggle completion of a Todo
   const toggleComplete = (id) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo
-      )
-    );
+    const updatedTodo = todos.find((todo) => todo.id === id);
+    updatedTodo.completed = !updatedTodo.completed;
+
+    fetch(`http://localhost:5000/todos/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedTodo),
+    }).then(() => {
+      setTodos(
+        todos.map((todo) =>
+          todo.id === id ? { ...todo, completed: updatedTodo.completed } : todo
+        )
+      );
+    });
   };
 
   // Function to toggle the edit mode of a Todo
@@ -39,11 +68,21 @@ const TodoWrapper = () => {
 
   // Function to edit the task of a Todo
   const editTask = (task, id) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, task, isEditing: false } : todo
-      )
-    );
+    const updatedTodo = { ...todos.find((todo) => todo.id === id), task };
+
+    fetch(`http://localhost:5000/todos/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedTodo),
+    }).then(() => {
+      setTodos(
+        todos.map((todo) =>
+          todo.id === id ? { ...todo, task, isEditing: false } : todo
+        )
+      );
+    });
   };
 
   return (
